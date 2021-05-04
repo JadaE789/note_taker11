c0c6c1e2c8db5ce303d7ee27d8dc33fb0387d650
Jada Desormeaux <JadaE789@gmail.com>
2021-05-03 20:18:26 -0400

    chnages

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,15 +4,15 @@ module.exports = function(app) {
   // HTML GET Requests
   // Below code handles when users "visit" a page.
   app.get('/notes', function(req, res) {
-    res.sendFile(path.join(__dirname, './02-Homework/Develop/public/notes.html'));
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
   });
 
   app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, './02-Homework/Develop/public/index.html'));
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
   });
 
   // If no matching route is found default to index
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, './02-Homework/Develop/public/index.html'));
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
   });
 };
